6a6f7df141728415f207defdae46988ba1fe70a1
Kerry Patrick <softwareliberationarmy@gmail.com>
2025-03-20 07:58:56 -0500

    homework: make backdrop click and no button close the modal

--- Section08/shared.js
+++ Section08/shared.js
@@ -9,3 +9,14 @@ for (const button of selectPlanButtons) {
     modal.style.display = 'block';
   });
 }
+
+backdrop.addEventListener('click', () => {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+});
+
+const noButton = modal.querySelector('.modal__action--negative');
+noButton.addEventListener('click', () => {
+  backdrop.style.display = 'none';
+  modal.style.display = 'none';
+});
